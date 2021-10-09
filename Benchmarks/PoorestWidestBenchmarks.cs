f244d46efdd997e6bbc14ac783a1731667f6f676
WhiteBlackGoose <wbg@angouri.org>
2021-10-09 21:54:52 +0300

    Benchmarks

--- Benchmarks/PoorestWidestBenchmarks.cs
+++ Benchmarks/PoorestWidestBenchmarks.cs
@@ -4,18 +4,22 @@ using System.Runtime.CompilerServices;
 using NoAlloq;
 using JM.LinqFaster;
 using NetFabric.Hyperlinq;
-
-namespace Benchmarks;
+using StructLinq;
 
 /*
  
-|              Method |       Mean |    Error |   StdDev |     Median | Code Size |  Gen 0 | Allocated |
-|-------------------- |-----------:|---------:|---------:|-----------:|----------:|-------:|----------:|
-| ClassicLinqCombined | 1,167.8 ns | 23.32 ns | 28.64 ns | 1,165.5 ns |   2,112 B | 0.0801 |     256 B |
-|     RefLinqCombined | 1,295.0 ns | 25.80 ns | 67.05 ns | 1,277.0 ns |   1,245 B |      - |         - |
-|     NoAlloqCombined | 1,345.6 ns | 26.72 ns | 44.65 ns | 1,329.2 ns |   2,114 B | 0.0267 |      88 B |
-|  LinqFasterCombined |   671.0 ns | 13.03 ns | 26.91 ns |   660.7 ns |   1,971 B | 0.4253 |   1,336 B |
-|   HyperlinqCombined |   664.0 ns | 13.09 ns | 14.55 ns |   661.1 ns |   1,506 B | 0.0277 |      88 B |
+|              Method |       Mean |    Error |   StdDev | Code Size |  Gen 0 | Allocated |
+|-------------------- |-----------:|---------:|---------:|----------:|-------:|----------:|
+| ClassicLinqCombined | 1,272.3 ns | 25.41 ns | 48.96 ns |   2,112 B | 0.0801 |     256 B |
+|     RefLinqCombined | 1,303.9 ns | 25.56 ns | 32.33 ns |   1,245 B |      - |         - |
+|     NoAlloqCombined | 1,362.8 ns | 26.61 ns | 54.96 ns |   2,114 B | 0.0267 |      88 B |
+|  LinqFasterCombined |   774.4 ns | 15.48 ns | 25.86 ns |   1,971 B | 0.4253 |   1,336 B |
+|   HyperlinqCombined |   702.6 ns | 10.77 ns | 16.45 ns |   1,506 B | 0.0277 |      88 B |
+|   ValueLinqCombined |   770.8 ns | 15.32 ns | 14.33 ns |   1,364 B | 0.0172 |      56 B |
+|      LinqAFCombined |   771.9 ns | 15.10 ns | 14.13 ns |   1,807 B | 0.0277 |      88 B |
+|  StructLinqCombined |   768.8 ns | 10.16 ns |  9.01 ns |     933 B | 0.0582 |     184 B |
+
+
  
  
  */
@@ -116,5 +120,82 @@ public class PWBenchmark
         foreach (var a in seq)
             res += a;
         return res;
+    }
+
+    [Benchmark]
+    public double ValueLinqCombined()
+    {
+        return Cistern.ValueLinq.RunValueLinq.ValueLinqCombined(arr, GetThing());
+    }
+
+    
+    [Benchmark]
+    public double LinqAFCombined()
+    {
+        return LinqAF.RunLinqAF.LinqAFCombined(arr, GetThing());
+    }
+
+    [Benchmark]
+    public double StructLinqCombined()
+    {
+        var res = 0.0;
+        var local = GetThing();
+        var seq = arr
+            .ToStructEnumerable()
+            .Select(c => c + 5)
+            .Where(c => c % 2 == 0)
+            .Select(c => c - 6.0 / local)
+            ;
+        foreach (var a in seq)
+            res += a;
+        return res;
+    }
+}
+
+namespace Cistern.ValueLinq
+{
+    struct ValueLinqCapture : Cistern.ValueLinq.IFunc<int, double>
+    {
+        private int capture;
+        public ValueLinqCapture(int capture)
+            => this.capture = capture;
+        public double Invoke(int c) => c - 6.0 / capture;
+    }
+
+    public static class RunValueLinq
+    {
+        public static double ValueLinqCombined(int[] arr, int local)
+        {
+            var res = 0.0;
+            var seq = arr
+                .Select(c => c + 5)
+                .Where(c => c % 2 == 0)
+                .Select(
+                new ValueLinqCapture(local),
+                default(double))
+                ;
+            foreach (var a in seq)
+                res += a;
+            return res;
+        }
+    }
+}
+
+namespace LinqAF
+{
+    public static class RunLinqAF
+    {
+        public static double LinqAFCombined(int[] arr, int local)
+        {
+            var res = 0.0;
+            var seq = arr
+                .Select(c => c + 5)
+                .Where(c => c % 2 == 0)
+                .Select(c => c - 6.0 / local)
+                ;
+            foreach (var a in seq)
+                res += a;
+            return res;
+        }
     }
 }
--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -11,5 +11,8 @@ Console.WriteLine($"o2: {b.RefLinqCombined()}");
 Console.WriteLine($"o3: {b.NoAlloqCombined()}");
 Console.WriteLine($"o4: {b.LinqFasterCombined()}");
 Console.WriteLine($"o5: {b.HyperlinqCombined()}");
+Console.WriteLine($"o6: {b.ValueLinqCombined()}");
+Console.WriteLine($"o7: {b.LinqAFCombined()}");
+Console.WriteLine($"o8: {b.StructLinqCombined()}");
 
 BenchmarkRunner.Run<PWBenchmark>();
