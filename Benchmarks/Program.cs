74346713d211acf57d1efcabc25601d766b10713
WhiteBlackGoose <wbg@angouri.org>
2021-10-12 23:00:12 +0300

    benchmarks improved

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -3,22 +3,22 @@
 using BenchmarkDotNet.Running;
 
 // 15873.600000000008
-// var b = new PWBenchmark();
-// Console.WriteLine($"o1: {b.ClassicLinqCombined()}");
-// Console.WriteLine($"o2: {b.RefLinqCombined()}");
-// Console.WriteLine($"o3: {b.NoAlloqCombined()}");
-// Console.WriteLine($"o4: {b.LinqFasterCombined()}");
-// Console.WriteLine($"o5: {b.HyperlinqCombined()}");
-// Console.WriteLine($"o6: {b.ValueLinqCombined()}");
-// Console.WriteLine($"o7: {b.LinqAFCombined()}");
-// Console.WriteLine($"o8: {b.StructLinqCombined()}");
+var b = new SelectWhereBenchmark();
+Console.WriteLine($"o1: {b.ClassicLinqCombined()}");
+Console.WriteLine($"o2: {b.RefLinqCombined()}");
+Console.WriteLine($"o3: {b.NoAlloqCombined()}");
+Console.WriteLine($"o4: {b.LinqFasterCombined()}");
+Console.WriteLine($"o5: {b.HyperlinqCombined()}");
+Console.WriteLine($"o6: {b.ValueLinqCombined()}");
+Console.WriteLine($"o7: {b.LinqAFCombined()}");
+Console.WriteLine($"o8: {b.StructLinqCombined()}");
 
-var b = new SelectWhereZipSumBenchmarks();
-Console.WriteLine($"o1: {b.RefLinqSum()}");
-Console.WriteLine($"o2: {b.ClassicLinqSum()}");
-Console.WriteLine($"o3: {b.HyperLinqSum()}");
-Console.WriteLine($"o4: {b.ValueLinqSum()}");
-Console.WriteLine($"o5: {b.LinqAFSum()}");
+// var b = new SelectWhereZipSumBenchmarks();
+// Console.WriteLine($"o1: {b.RefLinqSum()}");
+// Console.WriteLine($"o2: {b.ClassicLinqSum()}");
+// Console.WriteLine($"o3: {b.HyperLinqSum()}");
+// Console.WriteLine($"o4: {b.ValueLinqSum()}");
+// Console.WriteLine($"o5: {b.LinqAFSum()}");
 
 
 // var b = new PWBenchmark2();
@@ -35,6 +35,15 @@ Console.WriteLine($"o5: {b.LinqAFSum()}");
 // Console.WriteLine($"o4: {b.LinqAFBench()}");
 // 
 // 
-BenchmarkRunner.Run<SelectWhereZipSumBenchmarks>();
+
+if (args.Length is 0)
+    BenchmarkRunner.Run<SelectWhereBenchmark>();
+else
+{
+    var type = Type.GetType(args[0]);
+    if (type is null)
+        throw new($"Type {type} not found");
+    BenchmarkRunner.Run(type);
+}
 
 // Console.WriteLine(StackAllocBenchmark.Count(() => new PWBenchmark().RefLinqCombined()));
