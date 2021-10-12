f5ed6450d220894779f7f312f5907f8951aa21f2
WhiteBlackGoose <wbg@angouri.org>
2021-10-12 20:39:52 +0300

    Last added

---
+++ HonkPerf.NET/RefLinq/Extensions/Last.cs
@@ -0,0 +1,15 @@
+﻿namespace HonkPerf.NET.RefLinq;
+
+public static partial class ActiveLinqExtensions
+{
+    public static T Last<T, TEnumerator>(this RefLinqEnumerable<T, TEnumerator> seq)
+        where TEnumerator : IRefEnumerable<T>
+    {
+        if (!seq.enumerator.MoveNext())
+            ThrowHelpers.ThrowSequenceContainsNoElements();
+        var curr = seq.enumerator.Current;
+        while (seq.enumerator.MoveNext())
+            curr = seq.enumerator.Current;
+        return curr;
+    }
+}
---
+++ Tests/ExtensionsFunctionalTests/LastTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests.ExtensionsFunctionalTests;
+
+public class LastTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var seq =
+            new[] { 2, 3, 4 }
+            .ToRefLinq()
+            .Last();
+        Assert.Equal(4, seq);
+    }
+    [Fact]
+    public void Test3()
+    {
+        var seq =
+            new int[] { }
+            .ToRefLinq();
+        Assert.Throws<System.InvalidOperationException>(() => seq.Last());
+    }
+}
