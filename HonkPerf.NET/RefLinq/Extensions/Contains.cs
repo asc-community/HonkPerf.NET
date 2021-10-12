742ac63142e5baab2018ee6286b9bb24b0621b1d
WhiteBlackGoose <wbg@angouri.org>
2021-10-12 12:23:28 +0300

    Contains

---
+++ HonkPerf.NET/RefLinq/Extensions/Contains.cs
@@ -0,0 +1,15 @@
+﻿namespace HonkPerf.NET.RefLinq;
+
+public static partial class ActiveLinqExtensions
+{
+    public static bool Contains<T, TEnumerator>(this RefLinqEnumerable<T, TEnumerator> seq, T toFind)
+        where TEnumerator : IRefEnumerable<T>
+    {
+        foreach (var el in seq)
+        {
+            if (toFind is not null && toFind.Equals(el) || toFind is null && el is null)
+                return true;
+        }
+        return false;
+    }
+}
---
+++ Tests/ExtensionsFunctionalTests/ContainsTest.cs
@@ -0,0 +1,24 @@
+﻿namespace Tests.ExtensionsFunctionalTests;
+
+public class ContainsTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var seq =
+            new[] { 1, 2, 3, 4 }
+            .ToRefLinq()
+            .Contains(2);
+        Assert.True(seq);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var seq =
+            new[] { 1, 2, 3, 4 }
+            .ToRefLinq()
+            .Contains(5);
+        Assert.False(seq);
+    }
+}
