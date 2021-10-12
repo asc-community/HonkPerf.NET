8c9fd6e852faca559ae395a9edf3f38a040fab80
WhiteBlackGoose <wbg@angouri.org>
2021-10-12 12:35:03 +0300

    First added

---
+++ HonkPerf.NET/RefLinq/Extensions/First.cs
@@ -0,0 +1,13 @@
+﻿namespace HonkPerf.NET.RefLinq;
+
+public static partial class ActiveLinqExtensions
+{
+    public static T First<T, TEnumerator>(this RefLinqEnumerable<T, TEnumerator> seq)
+        where TEnumerator : IRefEnumerable<T>
+    {
+        if (seq.enumerator.MoveNext())
+            return seq.enumerator.Current;
+        ThrowHelpers.ThrowSequenceContainsNoElements();
+        return default!;
+    }
+}
---
+++ Tests/ExtensionsFunctionalTests/FirstTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests.ExtensionsFunctionalTests;
+
+public class FirstTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var seq =
+            new[] { 2, 3, 4 }
+            .ToRefLinq()
+            .First();
+        Assert.Equal(2, seq);
+    }
+    [Fact]
+    public void Test3()
+    {
+        var seq =
+            new int[] { }
+            .ToRefLinq();
+        Assert.Throws<System.InvalidOperationException>(() => seq.First());
+    }
+}
